c2bbc1e1cda150bc8acb2037f644af633d369a9d
Li Zheng <flyskywhy@gmail.com>
2020-05-03 15:36:38 +0800

    fix(react-native-btsig-telink): iOS SDK patch 1 ，解决 crc16() 重复定义的编译错误

--- SigMeshOC/SigMeshOC/LibHandle.m
+++ SigMeshOC/SigMeshOC/LibHandle.m
@@ -739,19 +739,19 @@ void sendStartOTA(){
     [Bluetooth.share writeOTAData:writeData];
 }
 
-extern unsigned short crc16 (unsigned char *pD, int len) {
-    static unsigned short poly[2]={0, 0xa001};
-    unsigned short crc = 0xffff;
-    int i,j;
-    for(j=len; j>0; j--) {
-        unsigned char ds = *pD++;
-        for(i=0; i<8; i++) {
-            crc = (crc >> 1) ^ poly[(crc ^ ds ) & 1];
-            ds = ds >> 1;
-        }
-    }
-    return crc;
-}
+// extern unsigned short crc16 (unsigned char *pD, int len) {
+//     static unsigned short poly[2]={0, 0xa001};
+//     unsigned short crc = 0xffff;
+//     int i,j;
+//     for(j=len; j>0; j--) {
+//         unsigned char ds = *pD++;
+//         for(i=0; i<8; i++) {
+//             crc = (crc >> 1) ^ poly[(crc ^ ds ) & 1];
+//             ds = ds >> 1;
+//         }
+//     }
+//     return crc;
+// }
 
 #pragma mark- Self customizer
 NSArray * changeObjToByte(NSString *string) {
